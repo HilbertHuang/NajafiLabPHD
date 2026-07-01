94fab8d50dd4d831ef28fc1b0c6d9f9fadc9ac40
Yicong (Hilbert) Huang <130207381+HilbertHuang@users.noreply.github.com>
2026-07-01 14:07:24 -0400

    Add files via upload

---
+++ 2p_imaging/2p_post_process_module_202404/run_main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=ImgProcess
+#SBATCH --account=gts-fnajafi3
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=64G
+#SBATCH --time=6:00:00
+#SBATCH --output=Report_%A-%a.out
+#SBATCH --mail-user=hilberthuang05@gatech.edu
+
+cd /storage/coda1/p-fnajafi3/0/yhuang887/Projects/2p_processing_pipeline_202401
+source activate suite2p
+python run_postprocess.py
---
+++ 2p_imaging/2p_post_process_module_202404/run_postprocess.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import numpy as np
+
+from modules import QualControlDataIO
+from modules import LabelExcInh
+from modules import DffTraces
+COMMANDLINE_MODE = False
+
+# parse arg input to number list.
+def get_qc_args(args):
+    range_skew = args.range_skew.split(',')
+    range_skew = np.array(range_skew, dtype='float32')
+    max_connect = np.array(args.max_connect, dtype='float32')
+    range_aspect = np.array(args.range_aspect, dtype='float32')
+    range_compact = args.range_compact.split(',')
+    range_compact = np.array(range_compact, dtype='float32')
+    range_footprint = args.range_footprint.split(',')
+    range_footprint = np.array(range_footprint, dtype='float32')
+    return [range_skew,
+            max_connect,
+            range_aspect,
+            range_compact,
+            range_footprint]
+
+# read saved ops.npy given a folder in ./results.
+def read_ops(session_data_path):
+    print('Processing {}'.format(session_data_path))
+    ops = np.load(
+        os.path.join(session_data_path, 'suite2p', 'plane0','ops.npy'),
+        allow_pickle=True).item()
+    ops['save_path0'] = os.path.join(session_data_path)
+    return ops
+
+if __name__ == "__main__":
+
+    if COMMANDLINE_MODE:
+        parser = argparse.ArgumentParser(description='Do not forget the everlasting love from Yicong!')
+        parser.add_argument('--session_data_path', required=True, type=str, help='The name of folder to save suite2p results.')
+        parser.add_argument('--range_skew',        required=True, type=str, help='The range of skew for quality control.')
+        parser.add_argument('--max_connect',       required=True, type=str, help='The maximum number of connectivity for quality control.')
+        parser.add_argument('--range_footprint',   required=True, type=str, help='The range of footprint for quality control')
+        parser.add_argument('--range_aspect',      required=True, type=str, help='The range of aspect ratio for quality control.')
+        parser.add_argument('--range_compact',     required=True, type=str, help='The range of compact for quality control.')
+        parser.add_argument('--diameter',          required=True, type=str, help='The diameter for cellpose on anatomical channel.')
+        args = parser.parse_args()
+        [range_skew,
+         max_connect,
+         range_aspect,
+         range_compact,
+         range_footprint] = get_qc_args(args)
+        ops = read_ops(args.session_data_path)
+    else:
+        '''
+        session_data_path_list = [
+            'C:/Users/yhuang887/Projects/passive_interval_oddball_202412/results/YH01VT/VTYH01_PPC_20250106_3331Random',
+        '''
+        session_data_path_list = [
+			'C:/Users/yhuang887/Projects/joystick_double_motor_timing_202601/2p/results/test/YH30VT_20260220_joystick', 'C:/Users/yhuang887/Projects/joystick_double_motor_timing_202601/2p/results/test/YH30VT_20260224_joystick', 'C:/Users/yhuang887/Projects/joystick_double_motor_timing_202601/2p/results/test/YH30VT_20260225_joystick', 'C:/Users/yhuang887/Projects/joystick_double_motor_timing_202601/2p/results/test/YH30VT_20260226_joystick_chemo', 'C:/Users/yhuang887/Projects/joystick_double_motor_timing_202601/2p/results/test/YH30VT_20260219_joystick'
+            ]
+            
+        for session_data_path in session_data_path_list:
+            ops = read_ops(session_data_path)
+            '''
+            # dendrites.
+            range_skew = [0,2]
+            max_connect = 2
+            range_aspect = [1.2,5]
+            range_footprint = [1,2]
+            range_compact = [1.06,5]
+            diameter = 6
+            '''
+            # neurons.
+            range_skew = [-5,5]
+            max_connect = 1
+            range_aspect = [0,5]
+            range_footprint = [1,2]
+            range_compact = [0,1.06]
+            diameter = 6
+
+            QualControlDataIO.run(
+                ops,
+                range_skew, max_connect, range_aspect, range_compact, range_footprint)
+
+            LabelExcInh.run(ops, diameter)
+        
+            DffTraces.run(ops, norm=True, correct_pmt=False)
