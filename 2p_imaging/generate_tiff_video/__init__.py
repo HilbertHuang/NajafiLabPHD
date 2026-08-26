dca2d243412ae87523f81d2acb3ce213dbb0839c
Yicong (Hilbert) Huang <130207381+HilbertHuang@users.noreply.github.com>
2026-08-26 18:32:43 -0400

    Delete 2p_imaging/generate_tiff_video directory

--- 2p_imaging/generate_tiff_video/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Generate denoised videos from two-photon Ch2 TIFF stacks."""
-
-from .video import process
-
-__all__ = ["process"]
--- 2p_imaging/generate_tiff_video/__main__.py
+++
@@ -1,7 +0,0 @@
-"""Run the TIFF-to-video workflow with ``python -m generate_tiff_video``."""
-
-from .cli import main
-
-
-if __name__ == "__main__":
-    main()
